7c10c7a30ad09b9d4e087557fd2dedef7624e2b2
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-12-12 14:08:39 +0530

    removed email valiadtion

--- controllers/FranchiseController.js
+++ controllers/FranchiseController.js
@@ -25,11 +25,6 @@ const generateUniqueCoupon = () => {
 const registerFranchise = asyncHandler(async (req, res) => {
     let { name,password, refBy, uplineId,mobileNumber,country,state,city,package,email } = req.body;
 
-    const franchise = await FranchiseModel.findOne({ email: email }); 
-    if(franchise){
-      return res.status(500).json({ message: "email already registered" });
-    }
-
     try {
         // Automatically generate a sequential code if not provided
         let code;
